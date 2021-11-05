6832e1a2b674481224cddcb60e6c1cd0df25cab2
Gelarehak <gelareh.akasheh@gmail.com>
2021-11-05 18:51:20 +0330

    part 1

--- app.js
+++ app.js
@@ -39,11 +39,32 @@ dat.innerHTML = currentyear(now);
 
 function showTemperature(response) {
   console.log(response.data);
+  document.querySelector("#city").innerHTML = response.data.name;
+  let cloudy = document.querySelector(".cloud");
+  cloudy.innerHTML = response.data.weather[0].main;
+  document.querySelector("#feellike").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+  document.querySelector("#humidity").innerHTML = Math.round(
+    response.data.main.humidity
+  );
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  let temp = document.querySelector(".temperature");
+  temp.innerHTML = Math.round(response.data.main.temp);
+  let iconelement = document.querySelector("#icon");
+  iconelement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+
+  celisusetemp = response.data.main.temp;
 }
 
 function searchcity(city) {
   let ApiKey = "f02bf8b085f29f8504d48daf88ff0017";
-  let Url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ApiKey}`;
+  let Url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ApiKey}&units=metric`;
   axios.get(Url).then(showTemperature);
 }
 function findcity(event) {
@@ -52,3 +73,27 @@ function findcity(event) {
   searchcity(city);
 }
 searchcity("Tehran");
+function displaycelisius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector(".temperature");
+  temperatureElement.innerHTML = Math.round(celisusetemp);
+  fahrenhietlink.classList.remove("active");
+  celisuselink.classList.add("active");
+}
+let celisuselink = document.querySelector("#celsiuse");
+celisuselink.addEventListener("click", displaycelisius);
+
+function displayfahrenhiet(event) {
+  event.preventDefault();
+  let fahrenhiettemp = (celisusetemp * 9) / 5 + 32;
+  let temperatureElement = document.querySelector(".temperature");
+  temperatureElement.innerHTML = Math.round(fahrenhiettemp);
+  celisuselink.classList.remove("active");
+  fahrenhietlink.classList.add("active");
+}
+
+let fahrenhietlink = document.querySelector("#fahrenhite");
+fahrenhietlink.addEventListener("click", displayfahrenhiet);
+
+let form = document.querySelector("#searchcity");
+form = addEventListener("submit", findcity);
